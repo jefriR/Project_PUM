913e945aef2ca7066d6c0380084dcc1832d7a908
jefriR <jefrironaldo48@yahoo.com>
2019-10-16 09:38:22 +0700

    + Detail Pum

--- app/Http/Controllers/API/DetailPumController.php
+++ app/Http/Controllers/API/DetailPumController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API;
 use App\DetailPum;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class DetailPumController extends Controller
