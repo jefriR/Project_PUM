e64fb25238d216c7e67a4447855845ad6b534caf
jefriR <jefrironaldo48@yahoo.com>
2019-10-28 17:05:36 +0700

    + report pum

---
+++ app/Http/Controllers/ReportAPI/ProsesReportController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\ReportAPI;
+
+use App\ReportPum;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class ProsesReportController extends Controller
+{
+    public function prosesReport(Request $request){
+        $validator = Validator::make($request->all(), [
+            'report_type'           => 'required',
+            'emp_id'                => 'required',
+            'dept_id'               => 'required',
+            'create_start_date'     => 'required',
+            'create_end_date'       => 'required',
+            'validate_start_date'   => 'required',
+            'validate_end_date'     => 'required',
+            'pum_status'            => 'required',
+            'response_status'       => 'required',
+            'detail_report'         => 'required',
+            'group_by'              => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error'=>true, 'message' => "Required Parameters are Missing or Empty"], 401);
+        }
+
+        $report_type        = $request->report_type;
+        $emp_id             = $request->emp_id;
+        $dept_id            = $request->dept_id;
+        $create_start_date  = $request->create_start_date;
+        $create_end_date    = $request->create_end_date;
+        $pum_status         = $request->pum_status;
+        $resp_status        = $request->response_status;
+        $validate_start_date = $request->validate_start_date;
+        $valdiate_end_date  = $request->validate_end_date;
+//        $detail_report      = $request->detail_report;
+//        $group_by           = $request->group_by;
+
+        if ($pum_status == 'ALL') {
+            $pum_status = ['N', 'APP1', 'APP2', 'APP3', 'APP4', 'A', 'I'];
+        }
+        if ($resp_status == 'ALL'){
+            $resp_status = ['N','F','P','I'];
+        }
+
+        $model      = new ReportPum();
+        $prosesData = $model->prosesData($emp_id, $dept_id, $create_start_date, $create_end_date, $pum_status, $resp_status, $validate_start_date, $valdiate_end_date);
+
+        return response()->json(['error' => false, 'message' => "Data Available", 'data' => $prosesData], 200);
+    }}
